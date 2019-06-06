af6cb416105efe3cf25bbc458958aa85451dc7ab
Kraysent <kraysent@gmail.com>
2019-06-06 22:38:40 +0300

    Added Save button in map creation

--- Gravitation_Modeling/Windows/CreateMapWindow.xaml.cs
+++ Gravitation_Modeling/Windows/CreateMapWindow.xaml.cs
@@ -1,14 +1,16 @@
 ﻿using Engine.Models;
 using Engine.ViewModel;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Threading;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using Vector2 = Engine.Models.Vector;
+using WinForms = System.Windows.Forms;
 
 namespace WPFUI.Windows
 {
@@ -23,6 +25,8 @@ namespace WPFUI.Windows
         public CreateMapWindow()
         {
             InitializeComponent();
+            _universe.Name = "Some map";
+            _universe.EnableTracers = false;
         }
         
         private void NewBodyButton_Click(object sender, RoutedEventArgs e)
@@ -87,6 +91,35 @@ namespace WPFUI.Windows
             {
                 Canvas.SetLeft(_bodies[i], _universe.Bodies[i].Coordinates.X * scale + xBias - _bodies[i].Width / 2);
                 Canvas.SetTop(_bodies[i], _universe.Bodies[i].Coordinates.Y * scale + yBias - _bodies[i].Height / 2);
+            }
+        }
+
+        private void SaveMapButton_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.SaveFileDialog dialog = new WinForms.SaveFileDialog();
+            string json;
+
+            dialog.Filter = "Map files (*.gmap)|*.gmap";
+            dialog.DefaultExt = "gmap";
+            dialog.AddExtension = true;
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrEmpty(dialog.FileName))
+            {
+                //We need to use these settings because in _universe.Bodies
+                //we have List that consists not only of MaterialPoints
+                //but also of PhysicalBody inherited from MaterialPoints.
+                //As a result, when we serialize - we serialize PhysicalBodies
+                //but when we deserialize - we deserialize MaterialPoints because
+                //Bodies is a List<MaterialPoint>, so we lose properties of Physical body.
+                //
+                //The same thing need to be done when we open universe
+                JsonSerializerSettings settings = new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All
+                };
+
+                json = JsonConvert.SerializeObject(_universe, settings);
+                File.WriteAllText(dialog.FileName, json);
             }
         }
 
